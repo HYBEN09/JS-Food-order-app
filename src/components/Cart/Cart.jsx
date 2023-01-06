6daf084efe9cef8fc1241deff3e43cdaa131e9f8
hyebeen byun <hyebeen2658@naver.com>
2023-01-06 23:57:25 +0900

    UI - Modal component 적용

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,25 +1,26 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 export const Cart = (props) => {
-  cartItems = (
+  const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, precw: 12.99 }].map((item) => (
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
-      <div>
+      <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button-alt"] type="button"}>Close</button>
-        <button className={classes.button} type="button">Order</button>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
