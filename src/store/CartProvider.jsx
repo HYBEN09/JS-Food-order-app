8af787a038015edf4d048a96ed765b628d3c601e
hyebeen byun <hyebeen2658@naver.com>
2023-01-13 22:20:13 +0900

    장바구니 리듀서 추가

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,15 +1,47 @@
+import { useReducer } from "react";
 import { CartContext } from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  //같은 음식 항목은 같이 그룹화 => 음식 기준으로 수량을 관리
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+
+    //장바구니 항목의 가격을 모두 더한 것.
+    const updatedTotalItems =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalItems,
+    };
+  }
+  return defaultCartState;
+};
 
 //CartContext 데이터를 관리하고, 그 컨텍스트를 접근하려는 모든 컴포넌트에 제공하는 것
 export const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
   //장바구니에 추가해야 할 항목을 얻습니다.
-  const addItemHandler = (item) => {};
-  const removeItemHandler = (id) => {};
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   //컨텍스트 자체를 위해 설정한 필드
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
